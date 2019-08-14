519b8dc1fcaace45b2d71850c4a9f48524f769f4
c-fitz <53093630+c-fitz@users.noreply.github.com>
2019-08-14 10:29:18 -0500

    Twitter proplem

--- R/linkage_minmax.R
+++ R/linkage_minmax.R
@@ -5,7 +5,6 @@ mu2 = c(1, 0)
 sigma1 = diag(0.4^2, 2)
 sigma2 = diag(0.4^2, 2)
 
-
 x1 = rmvnorm(250, mu1, sigma1)
 x2 = rmvnorm(250, mu2, sigma2)
 x = rbind(x1, x2)
@@ -26,15 +25,11 @@ c2 = cutree(h2, 2)
 D2 = data.frame(x, z = c2)
 ggplot(D2) + geom_point(aes(x=X1, y=X2, col=factor(z)))
 
-
 # Run hierarchical clustering with average linkage
 h3 = hclust(x_dist, method='average')
 c3 = cutree(h3, 2)
 D3 = data.frame(x, z = c3)
 ggplot(D3) + geom_point(aes(x=X1, y=X2, col=factor(z)))
-
-
-
 
 # But here's a different example where max produces counterintuitive results
 set.seed(84958)
@@ -42,7 +37,6 @@ mu1 = c(-1, 0)
 mu2 = c(1, 0)
 sigma1 = diag(0.1^2, 2)
 sigma2 = diag(0.45^2, 2)
-
 
 x1 = rmvnorm(250, mu1, sigma1)
 x2 = rmvnorm(250, mu2, sigma2)
@@ -61,7 +55,6 @@ h2 = hclust(x_dist, method='complete')
 c2 = cutree(h2, 2)
 D2 = data.frame(x, z = c2)
 ggplot(D2) + geom_point(aes(x=X1, y=X2, col=factor(z)))
-
 
 # Run hierarchical clustering with average linkage
 h3 = hclust(x_dist, method='average')
